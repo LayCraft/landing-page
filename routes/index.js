8ab40d28044d5d8043ae09457066ea679559beff
Curtis <curtis.laycraft@zoho.com>
2019-12-09 14:33:46 -0800

    add web fonts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'EMBC Landing Page' });
+	res.render('index', { title: 'Emergency Support Services' });
 });
 
 module.exports = router;
